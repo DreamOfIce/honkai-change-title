6c62cb97edc2c999e5ff1ad196820fc80d5bcf81
creeper2077 <mccreeper2077@gmail.com>
2021-11-04 13:42:39 +0800

    upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# 随机网站改变标题
+在离开&回到网站时将标题随机改为崩3名台词,并播放语音
+**语音声音较小,墙裂开大音量推荐在人群密集处体验\(￣︶￣*\)**
+**语音声音较小,墙裂开大音量推荐在人群密集处体验ψ(｀∇´)ψ**
+**语音声音较小,墙裂开大音量推荐在人群密集处体验φ(*￣0￣)**
+欢迎贡献新的语音ヾ(≧▽≦*)o
+[Github](https://github.com/Creeper2077/honkai-change-title) [Gitee](https://gitee.com/creeper-2077/honkai-change-title) [Codeberg](https://codeberg.org/Creeper2077/honkai-change-title)
+演示站点:[开大音量,点我O(∩_∩)O](https://random-api.creeper2077.online)
+
+![随机图片](https://random-api.creeper2077.online/img.php?game=bh3)
+
+## 注意
+从Chrome66开始,谷歌更新了音频播放策略,不允许自动播放音频,只能由用户手动触发.(也就是说,用户必须点一下才能播放音频)
+所以
+
+## 使用
+在网页内引用`title.min.js`即可:
+```html
+<script src='https://cdn.jsdelivr.net/gh/Creeper2077/honkai-change-title@main/title.min.js' ></script>
+```
+
+## 推荐
+我的其他项目:
+- [崩坏3壁纸合集](https://gitea.com/creeper2077/honkai3-wallpaper) 收录了崩3官方五年来发布的壁纸,共388张
+- [崩坏3&原神Random-API](https://github.com/Creeper2077/random-api) 随机返回崩坏3或原神的图片、音乐、视频&名台词
+- 敬请期待
+
+## 申明
+1. 音频提取自网络,版权归原作者所有;
+2. 因本项目造成的社死,友尽本人概不负责.
+
+## LICENSE
+[MIT](/LICENSE)
--- title.js
+++ title.js
@@ -1,16 +1,25 @@
 //define titles&audios
-const info = [{
+const list = [{
     title: 'Teriri的魔力耗完了,要好好休息才行呢w(ﾟДﾟ)w',
     audio: 'bronya.mp3'
 }, {
+    title: '我将,扭转万象┗|｀O′|┛ ',
+    audio: 'fuhua1.mp3'
+}, {
     title: "为世界上所有美好而战!",
-    audio: "kiana.mp3"
+    audio: "kiana1.mp3"
+}, {
+    title: '突然好想吃美少女味的泡面ヾ(•ω•`)o',
+    audio: 'kiana2.mp3'
+}, {
+    title: '我要喝芽衣的妹汁(￣y▽,￣)╭ ',
+    audio: 'kiana3.mp3'
+}, {
+    title: '舰长,我要吃儿童套餐(○｀ 3′○)',
+    audio: 'kiana4.mp3'
 }, {
     title: '我什么都做不到!(っ °Д °;)っ',
     audio: 'raiden1.mp3'
-}, {
-    title: '想吃美少女味的泡面ヾ(•ω•`)o',
-    audio: 'raiden2.mp3'
 }, {
     title: '苏醒吧,冰箱的主人(￣_,￣ )',
     audio: 'rita.mp3'
@@ -24,37 +33,51 @@ const info = [{
     title: '噫!忘掉刚才的声音╰（‵□′）╯',
     audio: 'seele2.mp3'
 }, {
-    title: '噢,你也想成为我的玩具吗φ(゜▽゜*)♪',
+    title: '噢?你也想成为我的玩具吗φ(゜▽゜*)♪',
     audio: 'seele3.mp3'
 }, {
-    titile: '把你Teriri掉哦(✿◕‿◕✿)',
+    title: '把你Teriri掉哦(✿◕‿◕✿)',
     audio: 'theresa.mp3'
 }];
 
 //variables
-var cdn = "https://cdn.jsdelivr.net/gh/Creeper2077/honkai-change-title@main/";
+var cdn = "https://cdn.jsdelivr.net/gh/Creeper2077/honkai-change-title@main/audio/";
 var time;
+var x;
 var norm_title = document.title;
 
 //random function
-function randomInfo() {
-    return info[Math.floor(Math.random() * info.length)];
+function randomItem() {
+    var i = Math.floor(Math.random() * list.length);
+    if (i == x) {
+        return randomItem();
+    }
+    x = i;
+    return list[i];
+}
+
+//add null audio
+window.onload = function () {
+    audio = document.createElement("AUDIO");
+    audio.setAttribute("src", `${cdn}null.mp3`);
+    audio.setAttribute("controls", "false");
+    audio.setAttribute("loop", "loop");
+    audio.setAttribute("autoplay", "autoplay");
 }
 
 //change title
-
 document.addEventListener('visibilitychange', function () {
     if (document.visibilityState == 'hidden') {
         clearTimeout(time);
-        var info = randomInfo();
-        document.title = info.title;
-        var mp3 = new Audio(cdn + info.audio);
+        var item = randomItem();
+        document.title = item.title;
+        var mp3 = new Audio(cdn + item.audio);
         mp3.play();
 
     } else {
-        var info = randomInfo();
-        document.title = info.title;
-        var mp3 = new Audio(cdn + info.audio);
+        var item = randomItem();
+        document.title = item.title;
+        var mp3 = new Audio(cdn + item.audio);
         mp3.play();
         time = setTimeout(function () {
             document.title = norm_title;
